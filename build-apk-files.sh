34a9fc5f20668965bea3aa231a70837e1f22211f
Wesley Shann <sshann95@outlook.com>
2020-07-14 19:32:59 +0200

    Make print less verbose

--- build-apk-files.sh
+++ build-apk-files.sh
@@ -53,24 +53,27 @@ passing_builds=()
 # Run the command `gradlew build` for all apps~treatment combination
 for treatment in $TREATMENTS_NAME; do
     for index in {1..$#APPS_NAME}; do
-        # Define the absolute path where the Android project to build is located
         start_app=$(date +%s)
+
+        # Define the absolute path where the Android project to build is located
         app_name=${APPS_NAME[index]}
         app_relative_path=${APPS_ANDROID_DIR[index]}
         app_dir="${PROJECT_DIR}/${treatment}/${app_name}/${app_relative_path}"
         cd $app_dir
 
+        treatment_app="${treatment} - ${app_name}"
+
         # Build apps via Gradle
-        print -P "%F{blue}%B% Building APK for the app ${app_name} with treatment ${treatment}."
+        print -P "%F{blue}%B% Building ${treatment_app}."
         ./gradlew build >>"${LOG_FILE_PATH}" 2>&1
 
         # Verifies if the build failed
         gradle_result=$?
         if (($gradle_result != 0)); then
-            builds_with_error+=("${treatment} - ${app_name}")
+            builds_with_error+=("${treatment_app}")
             print -P "%F{red}%B% Build failed."
         else
-            passing_builds+=("${treatment} - ${app_name}")
+            passing_builds+=("${treatment_app}")
             print -P "%F{green}%B% Build completed."
         fi
 
