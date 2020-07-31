eb8bc6eebc9577e065f7998c53215ed42117c7e3
Wesley Shann <sshann95@outlook.com>
2020-07-31 12:07:18 +0200

    Update script

--- assemble-apks.sh
+++ assemble-apks.sh
@@ -2,20 +2,20 @@
 
 # Build with ./gradlew assembleDebug
 PATHS_1=(
-    # Travel Mate
-  # "/home/sshann/Documents/thesis/subjects/baseline/Travel-Mate/Android"
+  # Travel Mate
+  "/home/sshann/Documents/thesis/subjects/baseline/Travel-Mate/Android"
   "/home/sshann/Documents/thesis/subjects/instrumented-nappa-greedy/Travel-Mate/Android"
   "/home/sshann/Documents/thesis/subjects/instrumented-nappa-tfpr/Travel-Mate/Android"
   "/home/sshann/Documents/thesis/subjects/instrumented-nappa-paloma/Travel-Mate/Android"
 
   # Materialistic
-  # "/home/sshann/Documents/thesis/subjects/baseline/materialistic"
+  "/home/sshann/Documents/thesis/subjects/baseline/materialistic"
   "/home/sshann/Documents/thesis/subjects/instrumented-nappa-greedy/materialistic"
   "/home/sshann/Documents/thesis/subjects/instrumented-nappa-tfpr/materialistic"
   "/home/sshann/Documents/thesis/subjects/instrumented-paloma/materialistic"
 
   # Hillffair
-  # "/home/sshann/Documents/thesis/subjects/baseline/Hillffair"
+  "/home/sshann/Documents/thesis/subjects/baseline/Hillffair"
   "/home/sshann/Documents/thesis/subjects/instrumented-nappa-greedy/Hillffair"
   "/home/sshann/Documents/thesis/subjects/instrumented-nappa-tfpr/Hillffair"
   "/home/sshann/Documents/thesis/subjects/instrumented-paloma/Hillffair"
@@ -41,13 +41,14 @@ echo ""
 
 
 
-# BUild with ./gradlew assembleFreeDebug
+# Fails for AntennaPod assemble it via Android Studio
+# Build with ./gradlew assembleFreeDebug
 PATHS_2=(
-  # AntennaPod
-  # "/home/sshann/Documents/thesis/subjects/baseline/AntennaPod/"
-  "/home/sshann/Documents/thesis/subjects/instrumented-nappa-greedy/AntennaPod/"
-  "/home/sshann/Documents/thesis/subjects/instrumented-nappa-tfpr/AntennaPod/"
-  "/home/sshann/Documents/thesis/subjects/instrumented-paloma/AntennaPod/"
+  # # AntennaPod
+  # # "/home/sshann/Documents/thesis/subjects/baseline/AntennaPod/"
+  # "/home/sshann/Documents/thesis/subjects/instrumented-nappa-greedy/AntennaPod"
+  # "/home/sshann/Documents/thesis/subjects/instrumented-nappa-tfpr/AntennaPod"
+  # "/home/sshann/Documents/thesis/subjects/instrumented-paloma/AntennaPod"
 )
 
 echo "Building PATHS_2 with assembleFreeDebug" 
@@ -56,7 +57,7 @@ for base_path_2 in "${PATHS_2[@]}"; do
   echo "-------------------------------------------------------------------------"
   echo "- Build for ${base_path_2}"
   echo "-------------------------------------------------------------------------"
-  if [ -d "${base_path_1}" ]; then
+  if [ -d "${base_path_2}" ]; then
     cd "${base_path_2}" || exit 1
     ./gradlew assembleDebug
     count=$((count + 1))
@@ -83,11 +84,12 @@ PATHS_3=(
   "/home/sshann/Documents/thesis/subjects/instrumented-nappa-tfpr/NewsBlur/clients/android/NewsBlur"
   "/home/sshann/Documents/thesis/subjects/instrumented-paloma/NewsBlur/clients/android/NewsBlur"
 
-  # UOB
-  # "/home/sshann/Documents/thesis/subjects/baseline/uob-timetable-android/uob"
-  "/home/sshann/Documents/thesis/subjects/instrumented-nappa-greedy/uob-timetable-android/uob"
-  "/home/sshann/Documents/thesis/subjects/instrumented-nappa-tfpr/uob-timetable-android/uob"
-  "/home/sshann/Documents/thesis/subjects/instrumented-paloma/uob-timetable-android/uob"
+  # Fails for UOB assemble via Android Studio 
+  # # UOB
+  # # "/home/sshann/Documents/thesis/subjects/baseline/uob-timetable-android/uob"
+  # "/home/sshann/Documents/thesis/subjects/instrumented-nappa-greedy/uob-timetable-android/uob"
+  # "/home/sshann/Documents/thesis/subjects/instrumented-nappa-tfpr/uob-timetable-android/uob"
+  # "/home/sshann/Documents/thesis/subjects/instrumented-paloma/uob-timetable-android/uob"
 )
 
 echo "Building PATHS_3 with assembleDebug" 
